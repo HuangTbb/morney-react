99e5a10577dcd7e30125bcb5357548b920aa0748
hjy <5336772+huangtobe@user.noreply.gitee.com>
2021-11-08 12:19:20 +0800

    导航栏布局

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,87 @@
 import React from 'react';
 
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from 'react-router-dom';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+const Main = styled.div`
+  flex-grow: 1;
+  overflow: auto;
+`;
+const Nav = styled.nav`
+  > ul {
+    display: flex; 
+    > li {
+      flex-grow: 1;
+      border: 1px solid;
+      text-align: center;
+      padding: 16px 0;
+    }
+  }
+`
 function App() {
   return (
-    <div>
-      Hello
-    </div>
+    <Router>
+      <Wrapper>
+        <Main>
+          <Switch>
+            <Route path="/tags">
+              <Tags/>
+            </Route>
+            <Route path="/money">
+              <Money/>
+            </Route>
+            <Route path="/statistics">
+              <Statistics/>
+            </Route>
+            <Redirect exact from="/" to="/money"/>
+            <Route path="*">
+              <NoMatch/>
+            </Route>
+          </Switch>
+        </Main>
+        <Nav>
+          <ul>
+            <li>
+              <Link to="/tags">标签</Link>
+            </li>
+            <li>
+              <Link to="/money">记账</Link>
+            </li>
+            <li>
+              <Link to="/statistics">统计</Link>
+            </li>
+          </ul>
+        </Nav>
+      </Wrapper>
+    </Router>
   );
+}
+
+function Tags() {
+  return <h2>Tags</h2>;
+}
+
+function Money() {
+  return <h2>Money</h2>;
+}
+
+function Statistics() {
+  return <h2>statistics</h2>;
+}
+
+function NoMatch() {
+  return <h2>当前路径不存在</h2>;
 }
 
 export default App;
