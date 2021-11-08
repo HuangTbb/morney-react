8bae457e3018a1c388c533f4bc5c1d5f73382246
hjy <5336772+huangtobe@user.noreply.gitee.com>
2021-11-08 16:33:00 +0800

    创建Icon组件

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,10 +1,12 @@
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
+import Icon from './Icons';
+
 
 const NavWrapper = styled.nav`
   line-height: 24px;
-  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+  box-shadow: 0 0 1px rgba(0, 0, 0, 0.25);
 
   > ul {
     display: flex;
@@ -12,7 +14,16 @@ const NavWrapper = styled.nav`
     > li {
       flex-grow: 1;
       text-align: center;
-      padding: 16px 0;
+      padding: 4px 0 2px 0;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      font-size: 10px;
+      > .icon {
+        width: 24px;
+        height: 24px;
+      }
     }
   }
 `;
@@ -22,12 +33,15 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
+          <Icon name="label"/>
           <Link to="/tags">标签</Link>
         </li>
         <li>
+          <Icon name="money"/>
           <Link to="/money">记账</Link>
         </li>
         <li>
+          <Icon name="statistics"/>
           <Link to="/statistics">统计</Link>
         </li>
       </ul>
