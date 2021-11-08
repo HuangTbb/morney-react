a22c7a70009a541d6c851d0a7b4b5a311d09740a
hjy <5336772+huangtobe@user.noreply.gitee.com>
2021-11-08 15:01:16 +0800

    封装Nav,添加svg文件

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
-
 import {
   HashRouter as Router,
   Switch,
   Route,
-  Link,
   Redirect,
 } from 'react-router-dom';
 import styled from 'styled-components';
+import Nav from './components/Nav';
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -18,17 +17,7 @@ const Main = styled.div`
   flex-grow: 1;
   overflow: auto;
 `;
-const Nav = styled.nav`
-  > ul {
-    display: flex; 
-    > li {
-      flex-grow: 1;
-      border: 1px solid;
-      text-align: center;
-      padding: 16px 0;
-    }
-  }
-`
+
 function App() {
   return (
     <Router>
@@ -50,19 +39,7 @@ function App() {
             </Route>
           </Switch>
         </Main>
-        <Nav>
-          <ul>
-            <li>
-              <Link to="/tags">标签</Link>
-            </li>
-            <li>
-              <Link to="/money">记账</Link>
-            </li>
-            <li>
-              <Link to="/statistics">统计</Link>
-            </li>
-          </ul>
-        </Nav>
+        <Nav/>
       </Wrapper>
     </Router>
   );
