4ab88506621402d099ec7f0ccb90d3fd5f5f92df
hjy <5336772+huangtobe@user.noreply.gitee.com>
2021-11-10 18:04:21 +0800

    完成label页面样式

--- src/views/Label.tsx
+++ src/views/Label.tsx
@@ -1,11 +1,60 @@
 import Layout from '../components/Layout';
-import React from 'react';
+import React, {useState} from 'react';
+import {useTags} from './useTags';
+import Icon from '../components/Icons';
+import styled from 'styled-components';
+import {Link,useRouteMatch} from 'react-router-dom';
+import {CreateTag} from '../components/CreateTag';
 
+const TagList = styled.ol`
+  padding:0 0 16px 16px;
+  margin: 14px 0;
+
+  > li {
+    border-bottom: 1px solid rgba(51, 51, 51, 0.1);
+    > a {
+      padding: 8px 6px 8px 0;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      .icon {
+        width: 2.2em;
+        height: 2.2em;
+        color: #666;
+      }
+    }
+    
+  }
+`;
+const Button = styled.button`
+  background: #F4C738;
+  width: 100px;
+  height: 28px;
+  line-height: 28px;
+  border-radius: 14px;
+  margin: 10px auto;
+  color: #fff;
+  display: block;
+`
 const Label = () => {
+  const {tags, setTags} = useTags();
+  const [visible, setVisible] = useState(false)
+  const match = useRouteMatch();
   return (
     <Layout>
-      <h2>Tags</h2>
+      <TagList>
+        {tags.map(tag => (
+          <li key={tag}>
+            <Link to={`${match.url}/${tag}`}>
+              <span className="oneLine">{tag}</span>
+              <Icon name="detail"/>
+            </Link>
+          </li>
+        ))}
+      </TagList>
+      <Button>新增标签</Button>
+      {/*<CreateTag value={visible} onChange={trueOrFalse => setVisible(trueOrFalse)}/>*/}
     </Layout>
   );
-}
+};
 export default Label;
--- src/views/Money/Tags.tsx
+++ src/views/Money/Tags.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import React, {useState} from 'react';
+import {useTags} from '../useTags';
 
 const TagsSection = styled.section`
   padding: 10px 0;
@@ -31,7 +32,7 @@ type Props = {
   onChange: (selected: string[]) => void;
 }
 const Tags: React.FC<Props> = (props: Props) => {
-  const [tags] = useState<string[]>(['衣','食','住','行'])
+  const {tags} = useTags()
   const selectedTags = props.value;
   const onToggleTag = (tag: string) => {
     const index = selectedTags.indexOf(tag)
