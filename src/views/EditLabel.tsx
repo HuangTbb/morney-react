de38caca7cb9975030a5eb33b8e3638a8b31b082
hjy <5336772+huangtobe@user.noreply.gitee.com>
2021-11-11 21:26:28 +0800

    优化updateTag和deleteTag

--- src/views/EditLabel.tsx
+++ src/views/EditLabel.tsx
@@ -3,12 +3,13 @@ import {EditInput} from './EditInput';
 import {useState} from 'react';
 import {GotoBack} from '../components/GotoBack';
 import {Button} from '../components/Button';
-import {useParams} from 'react-router-dom'
+import {useParams} from 'react-router-dom';
 import {useTags} from '../components/useTags';
 
 const EditLabelDiv = styled.div`
   background: #f5f5f5;
   height: 100vh;
+
   > .back {
     display: flex;
     align-items: center;
@@ -16,39 +17,44 @@ const EditLabelDiv = styled.div`
     box-shadow: 0 0 3px rgba(0, 0, 0, 0.1);
     margin-bottom: 13px;
   }
+
   > .editButtons {
     display: flex;
     justify-content: space-around;
     margin: 40px 0;
   }
-`
+`;
 type Params = {
   id: string
 }
 const EditLabel = () => {
-  const [newName, setNewName] = useState("")
-  const {findTag, updateTag} = useTags();
+  const [newName, setNewName] = useState('');
+  const {findTag, updateTag, deleteTag} = useTags();
   let {id} = useParams<Params>();
-  const tag = findTag(parseInt(id))
+  const tag = findTag(parseInt(id));
+  const tagContent = (tag: {id: number; name: string}) => (
+    <div>
+      <EditInput name="标签名" iconName="labelname" type="text"
+                 placeholder="请输入标签名"
+                 value={tag.name} onChange={() => setNewName(newName)}/>
+      <div className="editButtons">
+        <div onClick={() => {updateTag(tag.id, {name: newName});}}>
+          <Button name="修改"/>
+        </div>
+        <div onClick={() => {deleteTag(tag.id);}}>
+          <Button name="删除"/>
+        </div>
+      </div>
+    </div>
+  )
   return (
     <EditLabelDiv>
       <div className="back">
         <GotoBack/>
         <span>编辑标签</span>
       </div>
-      <EditInput name="标签名" iconName="labelname" type="text"
-                 placeholder="请输入标签名"
-                 value={tag.name} onChange={()=>setNewName(newName)} />
-      <div className="editButtons">
-        <div onClick={() => {
-          updateTag(tag.id, {name: newName})
-        }}>
-          <Button name="修改"/>
-        </div>
-
-        <Button name="删除"/>
-      </div>
+      {tag ? tagContent(tag): <div>tag不存在</div>}
     </EditLabelDiv>
-  )
-}
-export {EditLabel}
+  );
+};
+export {EditLabel};
